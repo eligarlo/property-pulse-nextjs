aeffcf9aaba10e81cf2f7097513a745f390857bc
Eliahu Garcia Lozano <eliahugarcialozano@gmail.com>
2024-08-07 19:11:54 +0300

    Initial commit

---
+++ src/lib/fonts.ts
@@ -0,0 +1,7 @@
+import { Poppins, Roboto_Mono } from 'next/font/google'
+
+export const poppins = Poppins({
+	subsets: ['latin'],
+	weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+	variable: '--font-poppins',
+})
