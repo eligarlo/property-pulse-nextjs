d568fa0ea3a697e1c6c3bcbe152eeaa28136e8bb
Eliahu Garcia Lozano <eliahugarcialozano@gmail.com>
2024-08-14 06:24:55 +0300

    Add recent properties section to the homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from '@/components/ui/Hero'
 import InfoBoxes from '@/components/ui/InfoBox/InfoBoxes'
+import HomeProperties from '@/components/ui/HomeProperties'
 
 const HomePage = () => {
 	return (
 		<>
 			<Hero />
 			<InfoBoxes />
+			<HomeProperties />
 		</>
 	)
 }
