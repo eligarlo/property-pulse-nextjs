6f6a75ac6a06d17200b9c787437509c3512c5520
Eliahu Garcia Lozano <eliahugarcialozano@gmail.com>
2024-08-26 08:09:20 +0300

    Add Aside bar Share buttons functionality

--- src/components/ui/property/ShareButtons.tsx
+++ src/components/ui/property/ShareButtons.tsx
@@ -1,4 +1,17 @@
-import { FaShare } from 'react-icons/fa'
+'use client'
+
+import {
+	FacebookShareButton,
+	TwitterShareButton,
+	TelegramShareButton,
+	WhatsappShareButton,
+	EmailShareButton,
+	FacebookIcon,
+	TwitterIcon,
+	TelegramIcon,
+	WhatsappIcon,
+	EmailIcon,
+} from 'react-share'
 
 import { PropertyType } from '@/types/property'
 
@@ -7,10 +20,33 @@ type ShareButtonsProps = {
 }
 
 const ShareButtons = ({ property }: ShareButtonsProps) => {
+	const shareUrl = `${process.env.NEXT_PUBLIC_DOMAIN}/properties/${property._id}`
+
 	return (
-		<button className='bg-orange-500 hover:bg-orange-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center'>
-			<FaShare className='mr-2' /> Share Property
-		</button>
+		<>
+			<h3 className='text-xl font-bold text-center pt-2'>Share this Property:</h3>
+			<div className='flex gap-3 justify-center pb-5'>
+				<FacebookShareButton url={shareUrl} hashtag={`#${property.type.replace(/\s/g, '')}ForRent`}>
+					<FacebookIcon size={40} round />
+				</FacebookShareButton>
+				<TwitterShareButton
+					url={shareUrl}
+					title={property.name}
+					hashtags={[`${property.type.replace(/\s/g, '')}ForRent`]}
+				>
+					<TwitterIcon size={40} round />
+				</TwitterShareButton>
+				<TelegramShareButton url={shareUrl} title={property.name}>
+					<TelegramIcon size={40} round />
+				</TelegramShareButton>
+				<WhatsappShareButton url={shareUrl} title={property.name} separator='::'>
+					<WhatsappIcon size={40} round />
+				</WhatsappShareButton>
+				<EmailShareButton url={shareUrl} subject={property.name} body={`Check out this property:`}>
+					<EmailIcon size={40} round />
+				</EmailShareButton>
+			</div>
+		</>
 	)
 }
 
